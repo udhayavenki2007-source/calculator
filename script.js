04da0c1b4c5cc5bcb5ee4fe3f2d8dfcd798713c0
udhayavenki2007-source <udhayavenki2007@gmail.com>
2026-04-10 14:43:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let display = document.getElementById("display");
 
-function appendVal(value) {
+function appendValue(value) {
     display.value += value;
 }
 
@@ -15,7 +15,7 @@ function deleteLast() {
 function calculate() {
     try {
         display.value = eval(display.value);
-    } catch (error) {
+    } catch {
         display.value = "Error";
     }
 }
